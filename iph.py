e1c1d4eb17a21329ab250c901d91fae827ab4f47
ruihou1 <66161995+ruihou1@users.noreply.github.com>
2022-04-12 11:04:23 +0800

    Update iph.py

--- iph.py
+++ iph.py
@@ -31,7 +31,7 @@ app.layout = html.Div([
                     # Use row and col to control vertical alignment of logo / brand
                     dbc.Row(
                         [dbc.Col(dbc.NavbarBrand(children=[html.H5("Calculation Tool for isolated post-challenge hyperglycemia(IPH)"),
-                            html.H5("单纯餐后血糖升高的计算工具"),className="ms-2")),
+                            html.H5("单纯餐后血糖升高的计算工具")],className="ms-2")),
                         ],
                         align="center",
                         className="g-0",
